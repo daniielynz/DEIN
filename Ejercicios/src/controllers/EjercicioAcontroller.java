15896dc811ed540f6c85cf75fb233ab8201d7cc1
daniielynz <danielyanezdm2@gmail.com>
2023-10-05 09:53:44 +0200

    UPDATE ejercicios
    Casi  terminado ejercicio A

--- Ejercicios/src/controllers/EjercicioAcontroller.java
+++ Ejercicios/src/controllers/EjercicioAcontroller.java
@@ -4,13 +4,22 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class EjercicioAcontroller {
 
@@ -62,6 +71,21 @@ public class EjercicioAcontroller {
     	//cbDeporte.setOnAction(event -> habilitarLista());
     	cbDeporte.setSelected(false);
     	lista.setDisable(true);
+    	
+    	// Poner TollTips a elementos
+    	Tooltip tp = new Tooltip();
+    	
+    	tp.setText("Elige los deportes que practiques");
+    	lista.setTooltip(tp);
+    	
+    	tp.setText("Valora del 1 al 10 tu aficcion a ir de compras");
+    	sldCompras.setTooltip(tp);
+    	
+    	tp.setText("Valora del 1 al 10 tu aficcion a ver la television");
+    	sldTelevision.setTooltip(tp);
+    	
+    	tp.setText("Valora del 1 al 10 tu aficcion a ir al cine");
+    	sldCine.setTooltip(tp);
     }
     
     @FXML
@@ -72,9 +96,9 @@ public class EjercicioAcontroller {
     @FXML
     void accionAceptar(ActionEvent event) {
     	String profesion = tfProfesion.getText();
-    	String NHermanos = tfHermanos.getText();
+    	int NHermanosNumerico = 0;
     	String edad = cbEdad.getSelectionModel().getSelectedItem();
-    	String sexo;
+    	String sexo = "";
     	if(rbHombre.isSelected()) {
     		sexo = rbHombre.getText();
     	}else if(rbMujer.isSelected()) {
@@ -82,7 +106,97 @@ public class EjercicioAcontroller {
     	}else if(rbOtro.isSelected()) {
     		sexo = rbOtro.getText();
     	}
+    	// Guardamos los deportes seleccionados de la lista
+    	ObservableList<String> deportesElegidos = lista.getSelectionModel().getSelectedItems();
+    	// Guardamos el valor de las aficciones
+    	double aficcionTelevision = sldTelevision.getValue();
+    	double aficcionCine = sldCine.getValue();
+    	double aficcionCompras = sldCompras.getValue();
+    	
+    	// Creamos variable donde iremos guardando sus errores
+    	String errores = "";
+    	// Validamos que el campo profesion no este vacio
+    	if(profesion.isEmpty()) {
+    		errores+= "Hay que rellenar el campo profesión\n";
+    	}
+    	// Validamos que el campo de Nº Hermanos no este vacio
+    	if(tfHermanos.getText().isEmpty()) {
+    		errores+= "Hay que rellenar el campo número de hermanos\n";
+    	}else {
+    		// Si no esta vacio, validamos si es numerico o no
+    		try {
+    			NHermanosNumerico = Integer.parseInt(tfHermanos.getText());
+    		}catch(NumberFormatException e) {
+    			errores+= "El numero de hermanos tiene que ser numerico\n";
+    		}
+    	}
+    	// Validamos que se haya elegido un deporte de la lista
+    	if(cbDeporte.isSelected() && deportesElegidos.isEmpty()){
+    		errores+= "Tienes que indicar los deportes que practicas";
+    	}
+    	// Creamos la ventana Emergente
+    	Alert ventanaEmergente = new Alert(null);
+    	ventanaEmergente.setTitle("TUS DATOS");
+    	
+    	if(errores.isEmpty()) {
+    		// Ponemos que la alerta sera de tipo Informacion
+    		ventanaEmergente.setAlertType(AlertType.INFORMATION);
+    		// Guardamos todos los datos del formulario
+    		String datos =  "Profesion:"+profesion+"\n"+
+		    				"Nº de hermanos:"+NHermanosNumerico+"\n";
+    		if(!edad.isEmpty()) {
+    			datos+= "Edad:"+edad+"\n";
+    		}
+    		if(!sexo.isEmpty()) {
+    			datos+= "Sexo:"+sexo+"\n";
+    		}
+    		if(cbDeporte.isSelected()) {
+    			datos+="Deportes que practicas:\n";
+    			for (String elemento : deportesElegidos) {
+                    datos+="\t"+elemento+"\n";
+                }
+    		}
+    		if(aficcionCompras != 0) {
+    			datos+= "Grado de aficción a las compras:"+(int)aficcionCompras+"\n";
+    		}
+    		if(aficcionTelevision != 0) {
+    			datos+= "Grado de aficción a ver la televisión:"+(int)aficcionTelevision+"\n";
+    		}
+    		if(aficcionCine != 0) {
+    			datos+= "Grado de aficción a ir al cine:"+(int)aficcionCine+"\n";
+    		}
+    		
+    		ventanaEmergente.setContentText(datos);
+    	}else {
+    		// Ponemos que la alerta sera de tipo Error
+    		ventanaEmergente.setAlertType(AlertType.ERROR);
+    		// Mostramos mensaje de error
+    		ventanaEmergente.setContentText(errores);
+    	}
+    	// Creamos el boton de Aceptar y su accion
+    	Button ocultarBtn = new Button("Aceptar");
+        ocultarBtn.setOnAction(e -> {
+        	ventanaEmergente.hide();
+        });
+    	ventanaEmergente.show();
     }
+    
+    public void mostrarDialogo() {
+        Stage ventanaEmergente = new Stage();
+        Button aceptarBtn = new Button("Aceptar");
+        aceptarBtn.setOnAction(e -> {
+            ventanaEmergente.close();
+        });
+        HBox contenedorRaiz = new HBox();
+        contenedorRaiz.setPadding(new Insets(20));
+        contenedorRaiz.setSpacing(20);
+        contenedorRaiz.getChildren().addAll(aceptarBtn);
+        Scene escena = new Scene(contenedorRaiz);
+        ventanaEmergente.setScene(escena);
+        ventanaEmergente.setTitle("TUS DATOS");
+        ventanaEmergente.show();
+    }
+   
 
     @FXML
     void accionCancelar(ActionEvent event) {
