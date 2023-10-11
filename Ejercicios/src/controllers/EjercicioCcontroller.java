3c8ae555a58e639bd7f17b1114d0547ca20895b7
daniielynz <danielyanezdm2@gmail.com>
2023-10-11 12:28:37 +0200

    UPDATE Ejercicio C
    Ejercicio C terminado y funciona

--- Ejercicios/src/controllers/EjercicioCcontroller.java
+++ Ejercicios/src/controllers/EjercicioCcontroller.java
@@ -52,40 +52,78 @@ public class EjercicioCcontroller {
 
     @FXML
     void initialize() {
-    	//ponemos events a la tabla 
+    	//ponemos events a la tabla
+    	tableInfo.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                Persona p = tableInfo.getSelectionModel().getSelectedItem();
+                tfNombre.setText(p.getNombre());
+                tfApellidos.setText(p.getApellidos());
+                tfEdad.setText(p.getEdad()+"");
+            }
+        });
     	
     	personas = FXCollections.observableArrayList();
     	// asignamos a la columna colNombre su cabera NOMBRE, asignado en FXML
 		colNombre.setCellValueFactory(new PropertyValueFactory<Persona,String>("nombre") );
 		colApellidos.setCellValueFactory(new PropertyValueFactory<Persona,String>("apellidos") );
 		colEdad.setCellValueFactory(new PropertyValueFactory<Persona,Integer>("edad") );
-		
-		//comportamiento de actualización por cada nueva entrada
-		colEdad.setCellFactory(col-> {
-				TableCell<Persona,Integer>cell = new TableCell<Persona, Integer>() {
-					@Override public void updateItem(Integer item, boolean empty) {
-						super.updateItem(item, empty);
-						this.setText(null);
-						this.setGraphic(null);
-						if (!empty) {
-							this.setText(Integer.toString(item));
-						}
-					}    					
-				};
-				cell.setAlignment(Pos.CENTER_RIGHT);
-				return cell;
-			
-		});
-    	
-    }
-    
-    @FXML
-    void accionRecuperarDatos(MouseEvent event) {
     	
     }
     
     @FXML
     void accionAceptar(ActionEvent event) {
+    	String errores = validarCampos();
+    	
+    	if(errores.isEmpty()) {
+    		// Si no hay errores creo el objeto persona
+    		Persona p = new Persona(tfNombre.getText(), tfApellidos.getText(), Integer.parseInt(tfEdad.getText()));
+    		// LLamo al metodo que se encarga de comprobar y añadir
+    		aniadirPersona(p);
+    		alertaInformacion("Se ha añadido a la persona correctamente");
+    	}else {
+    		alertaError(errores);
+    	}
+    }
+    
+    @FXML
+    private void accionEliminar(ActionEvent event) {
+    	// Guardo la persona seleccionada
+    	Persona p = tableInfo.getSelectionModel().getSelectedItem();
+    	// La borro de la lista
+    	borrarPersonaLista(p);
+    	alertaInformacion("Se ha eliminado a la persona seleccionada");
+    }
+    
+    @FXML
+    void accionModificar(ActionEvent event) {
+    	//Antes de modificar, validamos que los campos no tengan errores
+    	String errores = validarCampos();
+ 
+    	if(errores.isEmpty()) {
+    		// Si no da errores sacamos los datos
+    		String nombre = tfNombre.getText();
+        	String apellidos = tfApellidos.getText();
+        	int edad = Integer.parseInt(tfEdad.getText());
+        	// generamos una persona con los datos nuevos
+        	Persona p = new Persona(nombre, apellidos, edad);
+        	// borramos la persona seleccionada
+        	borrarPersonaLista(tableInfo.getSelectionModel().getSelectedItem());
+        	// Añadimos la persona seleccionada
+        	aniadirPersona(p);
+        	alertaInformacion("Se ha modificado la persona seleccionada");
+    	}else {
+    		// mostramos los errores
+    		alertaError(errores);
+    	}
+    }
+    
+    /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    		METODOS 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+    
+    private String validarCampos() {
     	String errores = "";
     	
     	// Obtenemos el dato del campo Nombre y validamos que no este vacio
@@ -106,59 +144,73 @@ public class EjercicioCcontroller {
 			errores+= "El numero de hermanos tiene que ser numerico\n";
 		}
     	
-    	if(errores.isEmpty()) {
-    		// Si no hay errores creo el objeto persona
-    		Persona p = new Persona(nombre, apellidos, edad);
-    		//tableInfo.refresh();
-    		if(!personas.contains(p)) {
-    			personas.add(p);
-    			// Mostramos la informacion en la ventana emergente
-        		Alert ventanaEmergente = new Alert(AlertType.INFORMATION);
-            	ventanaEmergente.setTitle("info");
-            	ventanaEmergente.setContentText("Persona añadida correctamente");
-            	// Creamos el boton de Aceptar y su accion
-            	Button ocultarBtn = new Button("Aceptar");
-                ocultarBtn.setOnAction(e -> {
-                	ventanaEmergente.hide();
-                });
-            	ventanaEmergente.show();
-            	tableInfo.setItems(personas);
-            	tableInfo.refresh();
-    		}else {
-    			// Mostramos ventana emergente de error
-        		Alert ventanaEmergente = new Alert(AlertType.ERROR);
-            	ventanaEmergente.setTitle("ERROR");
-            	ventanaEmergente.setContentText("Esa persona ya existe");
-            	// Creamos el boton de Aceptar y su accion
-            	Button ocultarBtn = new Button("Aceptar");
-                ocultarBtn.setOnAction(e -> {
-                	ventanaEmergente.hide();
-                });
-            	ventanaEmergente.show();
-    		}
+    	return errores;
+    }
+    
+    private void alertaError(String mensaje) {
+    	// Alerta de error con boton
+    	Alert ventanaEmergente = new Alert(AlertType.ERROR);
+    	ventanaEmergente.setTitle("info");
+    	ventanaEmergente.setContentText(mensaje);
+    	Button ocultarBtn = new Button("Aceptar");
+        ocultarBtn.setOnAction(e -> {
+        	ventanaEmergente.hide();
+        });
+        ventanaEmergente.show();
+    }
+    
+    private void alertaInformacion(String mensaje) {
+    	// Alerta de informacion con boton
+    	Alert ventanaEmergente = new Alert(AlertType.INFORMATION);
+    	ventanaEmergente.setTitle("info");
+    	ventanaEmergente.setContentText(mensaje);
+    	Button ocultarBtn = new Button("Aceptar");
+        ocultarBtn.setOnAction(e -> {
+        	ventanaEmergente.hide();
+        });
+        ventanaEmergente.show();
+    }
+    
+    private void borrarPersonaLista(Persona p) {
+    	// Compruebo si esa persona ya existe en la lista para poder borrarla
+    	if(personas.contains(p)) {
+    		// si la persona existe
+    		// la borramos de la lista y cargamos la tabla de nuevo
+    		personas.remove(p);
+    		tableInfo.setItems(personas);
+        	tableInfo.refresh();
+        	// Despues de borrar vacia los campos
+        	vaciarCampos();
     	}else {
-    		// Mostramos ventana emergente de error
-    		Alert ventanaEmergente = new Alert(AlertType.ERROR);
-        	ventanaEmergente.setTitle("ERROR");
-        	ventanaEmergente.setContentText(errores);
-        	// Creamos el boton de Aceptar y su accion
-        	Button ocultarBtn = new Button("Aceptar");
-            ocultarBtn.setOnAction(e -> {
-            	ventanaEmergente.hide();
-            });
-        	ventanaEmergente.show();
+    		// mensaje de error
+    		alertaError("Esa persona no existe");
     	}
     }
     
-    @FXML
-    void accionEliminar(ActionEvent event) {
-
+    private void aniadirPersona(Persona p) {
+    	// Comprueba si la persona ya existe en la lista
+    	if(!personas.contains(p)) {
+    		// si no existe
+    		// añade la nueva persona y cargamos la tabla de nuevo
+    		personas.add(p);
+    		tableInfo.setItems(personas);
+        	tableInfo.refresh();
+        	// Despues de borrar vacia los campos
+        	vaciarCampos();
+    	}else {
+    		// mensaje de error
+    		alertaError("Esa persona ya existe");
+    	}
+    }
+    
+    // Este metodo solo sirve para vaciar campos
+    private void vaciarCampos() {
+    	tfNombre.setText("");
+    	tfApellidos.setText("");
+    	tfEdad.setText("");
     }
 
-    @FXML
-    void accionModificar(ActionEvent event) {
-
-    }
+    
 }
 
 
