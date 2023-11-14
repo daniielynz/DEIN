74c049768205d7f8353bbd6ec01fe7bfefa24950
daniielynz <danielyanezdm2@gmail.com>
2023-11-14 14:18:35 +0100

    UPDATE Proyecto

---
+++ PROYECTO/Proyecto/src/controllers/ListadoController.java
@@ -0,0 +1,252 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ListadoController {
+
+	//
+    @FXML
+    private TableColumn<?, ?> colAlturaDeportista;
+
+    @FXML
+    private TableColumn<?, ?> colAnio;
+
+    @FXML
+    private TableColumn<?, ?> colAnioOlimpiada;
+
+    @FXML
+    private TableColumn<?, ?> colCalle;
+
+    @FXML
+    private TableColumn<?, ?> colCapacidad;
+
+    @FXML
+    private TableColumn<?, ?> colCiudad;
+
+    @FXML
+    private TableColumn<?, ?> colCiudadOlimpiada;
+
+    @FXML
+    private TableColumn<?, ?> colDeporteEvento;
+
+    @FXML
+    private TableColumn<?, ?> colDeportistaParticipacion;
+
+    @FXML
+    private TableColumn<?, ?> colEdadParticipacion;
+
+    @FXML
+    private TableColumn<?, ?> colEquipoParticipacion;
+
+    @FXML
+    private TableColumn<?, ?> colEventoParticipacion;
+
+    @FXML
+    private TableColumn<?, ?> colFotoDeportista;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colIdDeporte;
+
+    @FXML
+    private TableColumn<?, ?> colIdDeportista;
+
+    @FXML
+    private TableColumn<?, ?> colIdEquipo;
+
+    @FXML
+    private TableColumn<?, ?> colIdEvento;
+
+    @FXML
+    private TableColumn<?, ?> colIdOlimpiada;
+
+    @FXML
+    private TableColumn<?, ?> colInicialesEquipo;
+
+    @FXML
+    private TableColumn<?, ?> colMedallaParticipacion;
+
+    @FXML
+    private TableColumn<?, ?> colNSocios;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colNombreDeporte;
+
+    @FXML
+    private TableColumn<?, ?> colNombreDeportista;
+
+    @FXML
+    private TableColumn<?, ?> colNombreEquipo;
+
+    @FXML
+    private TableColumn<?, ?> colNombreEvento;
+
+    @FXML
+    private TableColumn<?, ?> colNombreOlimpiada;
+
+    @FXML
+    private TableColumn<?, ?> colNumero;
+
+    @FXML
+    private TableColumn<?, ?> colOlimpiadaEvento;
+
+    @FXML
+    private TableColumn<?, ?> colPais;
+
+    @FXML
+    private TableColumn<?, ?> colPesoDeportista;
+
+    @FXML
+    private TableColumn<?, ?> colSexoDeportista;
+
+    @FXML
+    private ToggleGroup groupRb;
+
+    @FXML
+    private RadioButton rbDeportes;
+
+    @FXML
+    private RadioButton rbEquipos;
+
+    @FXML
+    private RadioButton rbEventos;
+
+    @FXML
+    private RadioButton rbOlimpiadas;
+
+    @FXML
+    private RadioButton rbParticipaciones;
+
+    @FXML
+    private RadioButton rbPrivados;
+
+    @FXML
+    private TableView<?> tablaDeporte;
+
+    @FXML
+    private TableView<?> tablaDeportista;
+
+    @FXML
+    private TableView<?> tablaEquipos;
+
+    @FXML
+    private TableView<?> tablaEventos;
+
+    @FXML
+    private TableView<?> tablaOlimpiadas;
+
+    @FXML
+    private TableView<?> tablaParticipaciones;
+
+    @FXML
+    private TableView<?> tableAeropuertosPrivados;
+
+    @FXML
+    private TextField tfBuscarPorNombre;
+
+    @FXML
+    private Label tfListado;
+
+    @FXML
+    void accionAniadirDeporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBorrarDeporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBorrarDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBorrarEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBorrarEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBorrarOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBorrarParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEditarDeporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEditarDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEditarEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEditarEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEditarOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEditarParticipacion(ActionEvent event) {
+
+    }
+
+}
